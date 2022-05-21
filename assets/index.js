6feed28e04fb9ddfd055f3b6d64526c708482bb8
guitarkeegan <keegananglim@gmail.com>
2022-05-21 15:01:19 -0700

    start button changes css properties for start-card and start-button

---
+++ assets/index.js
@@ -0,0 +1,12 @@
+document.getElementById("start-button").addEventListener("click", function(){
+    console.log("I've been clicked!")
+    document.getElementById("start-card").style.setProperty("display", "none");
+    document.getElementById("start-button").style.setProperty("display", "none");
+    document.getElementById("question-card").style.setProperty("display", "block");
+});
+    
+    // document.getElementById("start-card").style({display: none});
+
+
+
+    
